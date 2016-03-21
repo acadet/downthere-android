41554bdc4e9dd93c943c73699d249e46f7938cfc
Adrien Cadet <acadet@live.fr>
2016-03-21 15:10:20 -0700

    Adds configuration for release

--- app/src/debug/java/com/adriencadet/downthere/ApplicationConfiguration.java
+++ app/src/debug/java/com/adriencadet/downthere/ApplicationConfiguration.java
@@ -5,7 +5,9 @@ package com.adriencadet.downthere;
  * <p>
  */
 public class ApplicationConfiguration {
-    public final String SERVER_ENDPOINT = "http://www42023.coshx.com";
+    public final String SERVER_ENDPOINT = "http://www38401.coshx.com";
 
     public final int PICTURE_GRID_CACHING_MINUTES = 1;
+
+    public final int TEXT_FILE_CACHING_MINUTES = 1;
 }
